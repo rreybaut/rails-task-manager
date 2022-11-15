6eb545a62faaeb0ce5659ff2313f524c243b10b9
raphael reybaut <rreybaut@gmail.com>
2022-11-15 17:05:44 +0100

    almost done need checkbox displayed on index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # See all tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # See details about one task
+  def show
+  end
+  # Create a task
+  def create
+    @task = Task.create(task_params)
+
+    redirect_to tasks_path
+  end
+  def new
+    @task = Task.new
+  end
+# Eit a task
+  def update
+  @task = Task.find(params[:id])
+  @task.update(task_params)
+  redirect_to tasks_path
+  end
+# Update a task
+  def edit
+  end
+  # Destroy a task
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'tasks', to: 'tasks#index'
   # Defines the root path route ("/")
+  # Crée un task
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create', as: :task_create
+  # updater un task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id', to: 'tasks#update', as: :task_update
+  # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
+  # Lire les informations d'un task
+  get '/tasks/:id', to: 'tasks#show', as: :task
   # root "articles#index"
 end
